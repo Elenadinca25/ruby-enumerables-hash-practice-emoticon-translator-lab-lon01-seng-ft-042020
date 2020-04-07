9afd6dce8cc60c84752ed84acf95738cf65590ae
Elena Dinca <elenadinca25@gmail.com>
2020-04-07 23:17:37 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,6 +6,8 @@ def load_library(path)
   YAML.load_file(path).transform_values{|v| {english: v[0], japanese: v[1]}}
 end
 
+
+
 def get_japanese_emoticon(path, emoticon)
   emoticons = load_library(path)
   emoticons.each do |meaning, translations|
@@ -15,6 +17,8 @@ def get_japanese_emoticon(path, emoticon)
   end
   'Sorry, that emoticon was not found'
 end
+
+
 
 def get_english_meaning(path, emoticon)
   emoticons = load_library(path)
